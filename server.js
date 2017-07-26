406e701a6064c0a61552ad31760f0c3fa5608264
hzluziling <hzluziling@corp.netease.com>
2017-07-26 14:35:26 +0800

    update server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+// require: express socket.io shortid
 
-var io = require('socket.io')(3001);
+var io = require('socket.io')(3000);
 var shortid = require('shortid');
 
 console.log('server started');
 
+// console.log(shortid.generate());
+
 var snakes = [];
 
 io.on('connection', function (socket) {
+
     var thisSnakeId = shortid.generate();
 
-    snakes[thisSnakeId] = {
+    var snake = {
         id: thisSnakeId,
         head_x: 0,
         head_z: 0,
@@ -19,10 +23,15 @@ io.on('connection', function (socket) {
         speed: 'normal',
     };
 
-    socket.emit('init', snakes[thisSnakeId]);
+    snakes[thisSnakeId] = snake;
+
+    socket.emit('init', snake);
+    socket.broadcast.emit('init_another', snake)
+
+    socket.broadcast.emit('req_sync');
 
     socket.on('sync', function (data) {
-        console.log('sync', JSON.stringify(data));
+        // console.log('sync', JSON.stringify(data));
 
         var snake = snakes[thisSnakeId];
 
@@ -33,12 +42,37 @@ io.on('connection', function (socket) {
         snake.dir = data['dir'];
         snake.speed = data['speed'];
 
-        socket.emit('sync, snake');
+        socket.emit('sync', snake);
+        socket.broadcast.emit('sync_another', snake);
+
     });
+
+    // console.log('client connected, broadcast spawn, id:', thisSnakeId);
+
+    // for (var snakeId in snakes) {
+    //  if (snakeId == thisSnakeId)
+    //      continue;
+
+    //  socket.emit('spawn', snakes[snakeId]);
+    // }
+
+    // socket.emit('spawn');
+
+    // socket.broadcast.emit('spawn');
+
+    // io.sockets.emit('spawn');
+
+    // socket.on('move', function (data) {
+    //  data.id = thisSnakeId;
+    //  console.log('client moved', JSON.stringify(data));
+    // });
 
     socket.on('disconnect', function () {
         console.log('client disconnected');
 
         delete snakes[thisSnakeId];
+
+        // socket.broadcast.emit('disconnected', {id: thisSnakeId});
     });
 });
+
